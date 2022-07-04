60c1b089084831ab9d14244162f9bd6c582c94f4
Likhita <leenupandagre24@gmail.com>
2022-07-05 00:27:05 +0530

    All UCs Day 9,10,27,28

---
+++ AddressBook/AddressBook/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+namespace addressbook
+{
+    public class program
+    {
+
+        public static List<contacts> person = new List<contacts>();
+
+
+        public static void Main(string[] args)
+        {
+            Person p = new Person();
+
+
+            // p.createcontacts();
+            // p.editcontacts();
+            // p.deletecontacts();
+            // p.displaycontacts();
+            // p.addmulticontacts();
+            // p.multiaddressbook();
+            // p.createcontacts();
+            // p.SearchByCityorState();
+            // p.displaycontacts();
+            // p.displayByCityOrState();
+            // p.getCount();
+            // p.WriteTextFile();
+            // p.ReadTextFile();
+            // p.WriteCSVFile();
+            // p.ReadCSVFile();
+            p.JSONSerialization();
+            p.JSONDeserialization();
+
+
+
+            p.displaycontacts();
+
+
+
+
+
+        }
+    }
+
+}
